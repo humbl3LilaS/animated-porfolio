8064099121834713d8301dc8c51e97cc13dd1fb6
humbl3LilaS <sabishinekobebe@gmail.com>
2024-05-02 22:11:10 +0630

    Add: Moving Text Function

--- src/components/MovingText.jsx
+++ src/components/MovingText.jsx
@@ -1,11 +1,30 @@
+import { motion } from "framer-motion";
+
+const sliderVariant = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-200%",
+    transition: {
+      duration: 20,
+      repeat: Infinity,
+      repeatType: "mirror",
+    },
+  },
+};
+
 export default function MovingText() {
   return (
-    <p
+    <motion.p
       className={
-        "absolute bottom-0 text-[50vh] leading-[1]  whitespace-nowrap text-moving-text"
+        "w-1/2 absolute bottom-0 text-[50vh] leading-[1]  whitespace-nowrap text-moving-text"
       }
+      variants={sliderVariant}
+      initial="initial"
+      animate="animate"
     >
       Junior Front-End Developer React Developer
-    </p>
+    </motion.p>
   );
 }
