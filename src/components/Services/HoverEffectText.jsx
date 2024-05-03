7043699bbc14b584af6da69088637a2a64cdd326
humbl3LilaS <sabishinekobebe@gmail.com>
2024-05-03 22:55:10 +0630

    Add: Text Hover Effect in Service CTA

--- src/components/Services/HoverEffectText.jsx
+++ src/components/Services/HoverEffectText.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
 
 export default function HoverEffectText({ children }) {
-  return <motion.b>{children}</motion.b>;
+  return <motion.b whileHover={{ color: "orange" }}>{children}</motion.b>;
 }
