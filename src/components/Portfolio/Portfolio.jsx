806529f1dbd1fa003ac89b38cbf4681460730297
humbl3LilaS <sabishinekobebe@gmail.com>
2024-05-04 20:36:26 +0630

    Add: Parallax effect in PortfolioItems

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,6 +2,34 @@ import { useRef } from "react";
 import PortfolioItem from "./PortfolioItem";
 import ProgressBar from "./ProgressBar";
 import { motion, useScroll, useSpring } from "framer-motion";
+
+const items = [
+  {
+    id: 1,
+    title: "React Commerce",
+    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+  },
+  {
+    id: 2,
+    title: "Next.js Blog",
+    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+  },
+  {
+    id: 3,
+    title: "Vanilla JS App",
+    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+  },
+  {
+    id: 4,
+    title: "Music App",
+    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+  },
+];
+
 export default function Portfolio() {
   const ref = useRef();
   const { scrollYProgress } = useScroll({
@@ -14,9 +42,9 @@ export default function Portfolio() {
   return (
     <div ref={ref} className={"relative"}>
       <ProgressBar animation={scaleX} />
-      <PortfolioItem />
-      <PortfolioItem />
-      <PortfolioItem />
+      {items.map((item) => (
+        <PortfolioItem data={item} key={item.title} />
+      ))}
     </div>
   );
 }
--- src/components/Portfolio/PortfolioItem.jsx
+++ src/components/Portfolio/PortfolioItem.jsx
@@ -1,7 +1,35 @@
-export default function PortfolioItem() {
+import { motion, useScroll, useTransform } from "framer-motion";
+import ClickyBtn from "./ClickyBtn";
+import { useRef } from "react";
+
+export default function PortfolioItem({ data }) {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({ target: ref });
+
+  const y = useTransform(scrollYProgress, [0, 1], [120, -120]);
+
   return (
-    <section className="h-screen snap-center">
-      <h1>hehe</h1>
+    <section className="h-screen flex items-center justify-center  snap-center ">
+      <div
+        className={
+          "max-w-desktop mx-auto flex items-center gap-x-10 overflow-hidden "
+        }
+        ref={ref}
+      >
+        <div>
+          <img src={data?.img} alt="a photo" />
+        </div>
+        <motion.article
+          className={"flex flex-col justify-between gap-y-8"}
+          style={{ y }}
+        >
+          <h2 className={"text-4xl font-bold tracking-wide"}>{data?.title}</h2>
+          <p className={"text-lg"}>{data?.desc}</p>
+          <ClickyBtn className={"block w-1/2 bg-orange-400 py-2 rounded"}>
+            See Full Demo
+          </ClickyBtn>
+        </motion.article>
+      </div>
     </section>
   );
 }
